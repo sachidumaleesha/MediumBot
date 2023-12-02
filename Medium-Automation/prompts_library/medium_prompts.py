6a689dd19b5bed0e6852d5dadead2f973898f066
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-12-02 22:55:52 +0530

    Add files via upload

---
+++ Medium-Automation/prompts_library/medium_prompts.py
@@ -0,0 +1,3 @@
+medium_story_prompt = """Generate a blog post from the provided [video script]. Utilize formatting elements such as H1 and H2 headers, paragraphs, and bullet points to transform the script. Ensure the original sentences remain unchanged; your task is limited to rectifying any grammatical errors and conducting a comprehensive spellcheck.
+
+video script = {video_script}"""
