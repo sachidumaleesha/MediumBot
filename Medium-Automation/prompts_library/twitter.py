923edada8a79f4eed06d14710d08b3a180d367bd
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-12-02 22:57:43 +0530

    Delete Medium-Automation directory

--- Medium-Automation/prompts_library/twitter.py
+++
@@ -1,4 +0,0 @@
-twitter_thread_generator_prompt = """Act as if you're a social media expert.
-Give me a 10 tweet thread about [{topic}].
-The thread should be optimised for virality and contain hashtags and emoticons.
-Each tweet should not exceed 280 characters in length."""
